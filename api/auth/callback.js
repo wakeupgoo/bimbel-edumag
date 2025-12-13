1b5c38d55a329b0d0681f79b89f8f9696533b796
bimbel-edumag <edumag27@gmail.com>
2025-12-13 14:54:51 +0800

    semoga saja

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,8 +1,31 @@
-export default function handler(req, res) {
-  const code = req.query.code;
+export default async function handler(req, res) {
+  const { code } = req.query;
 
   if (!code) {
     res.status(400).send("Missing code");
+    return;
+  }
+
+  const tokenRes = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      })
+    }
+  );
+
+  const tokenData = await tokenRes.json();
+
+  if (!tokenData.access_token) {
+    res.status(500).send("Failed to obtain access token");
     return;
   }
 
@@ -12,19 +35,17 @@ export default function handler(req, res) {
 <html>
   <body>
     <script>
-      (function () {
-        if (window.opener) {
-          window.opener.postMessage(
-            {
-              type: "authorization_response",
-              provider: "github",
-              code: "${code}"
-            },
-            window.location.origin
-          );
-          window.close();
-        }
-      })();
+      if (window.opener) {
+        window.opener.postMessage(
+          {
+            type: "authorization_response",
+            provider: "github",
+            token: "${tokenData.access_token}"
+          },
+          window.location.origin
+        );
+        window.close();
+      }
     </script>
   </body>
 </html>
