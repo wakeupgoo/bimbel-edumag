d3833b8fcf4f21d1b682df64b2d016bbe208b0d8
bimbel-edumag <edumag27@gmail.com>
2025-12-13 14:30:37 +0800

    final fixing

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,3 +1,30 @@
-export default function handler(req, res) {
-  res.status(200).send("CALLBACK OK");
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    res.status(400).send("Missing code");
+    return;
+  }
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.status(200).send(`
+<!doctype html>
+<html>
+  <body>
+    <script>
+      if (window.opener) {
+        window.opener.postMessage(
+          {
+            type: "authorization_response",
+            provider: "github",
+            code: "${code}"
+          },
+          "*"
+        );
+        window.close();
+      }
+    </script>
+  </body>
+</html>
+  `);
 }
--- api/auth/index.js
+++ api/auth/index.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
-  res.status(200).send("AUTH OK");
+  const params = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    redirect_uri: process.env.OAUTH_CALLBACK_URL,
+    scope: "repo"
+  });
+
+  res.writeHead(302, {
+    Location: `https://github.com/login/oauth/authorize?${params.toString()}`
+  });
+  res.end();
 }
